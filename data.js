2d8933e2cd5a7684e16ddc819d5f9ce0c4f3a9f9
Olga777930 <ok748681@gmail.com>
2025-04-27 13:04:24 +0200

    360

---
+++ data.js
@@ -0,0 +1,201 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "КОМНАТА ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0621863341755162,
+        "pitch": 0.0037829624336165324,
+        "fov": 1.476833843712925
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.871468373254711,
+          "pitch": 0.1338140364791247,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "КУХНЯ ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.8508239957112913,
+        "pitch": 0.020783683342987302,
+        "fov": 1.476833843712925
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.093285633056123,
+          "pitch": 0.06487707464923886,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "БАЛКОН ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.11954792011597,
+        "pitch": 0.08340104648737423,
+        "fov": 1.476833843712925
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.752716141181063,
+          "pitch": 0.3057935174085209,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "ПРИХОЖАЯ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.7795098642089915,
+        "pitch": -0.024101428118060042,
+        "fov": 1.476833843712925
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4482100275546106,
+          "pitch": 0.04260905127180692,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "ВАННА",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0283013395039866,
+        "pitch": 0.002205355421297739,
+        "fov": 1.476833843712925
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.036683237403979,
+          "pitch": 0.009631712121288771,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
